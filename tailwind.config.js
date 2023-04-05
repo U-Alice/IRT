d7e9e0451f016b46d76f8ca74ce0df1be08e2aa4
U-Alice <umugwanezaalice22@gmail.com>
2023-04-05 17:01:11 +0200

    new updates on home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'regal-green' : 'rgba(77, 189, 151, 0.53)',
+        'trans-blue' : 'rgba(2, 86, 119, 0.78)'
+      }
+    },
   },
   plugins: [],
 }
