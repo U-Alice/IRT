806391bd0865298450bf4ff7cfa34214b5f593ac
U-Alice <umugwanezaalice22@gmail.com>
2023-04-08 08:11:02 +0200

    completed contact us

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     extend: {
       colors: {
         'regal-green' : 'rgba(77, 189, 151, 0.53)',
+        'green' : 'rgba(77, 189, 151, 0.83)',
         'trans-blue' : 'rgba(2, 86, 119, 0.78)',
         'blue-linear' : 'rgba(2, 86, 119, 1)'
       },
